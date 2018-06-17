6a3d85afd3cbf76a54a53ee8ed93f6755c811a53
Ben Krege <jbkrege@gmail.com>
2018-06-17 16:09:51 -0500

    restructuring json

--- js/marcia.js
+++ js/marcia.js
@@ -12,110 +12,110 @@ pad = String(window.innerHeight/2 - (document.getElementById("contact").offsetHe
 document.getElementById("contact").style.paddingTop = pad;
 document.getElementById("contact").style.paddingBottom = pad;
 
-paintings = [ 
-  {
-    "name": "Red Square",
-    "img": "./img/redSquare.jpg",
-    "year": "2018",
-    "medium": "Acrylic paint on canvas",
-    "size": "20 x 20 inches"
-  },
+all_paintings = {
+  "colorWeave": 
+    [ 
+      {
+        "name": "Square One I",
+        "img": "./img/squareOneI.jpg",
+        "year": "2010",
+        "medium": "Acrylic paint on canvas",
+        "size": "20 x 20 inches"
+      }, 
+      {
+        "name": "Square One II",
+        "img":  "./img/squareOneII.jpg",
+        "year": "2018",
+        "medium": "Acrylic paint on canvas",
+        "size": "40 x 40 inches"
+      },
+      {
+        "name": "Red Square",
+        "img": "./img/redSquare.jpg",
+        "year": "2018",
+        "medium": "Acrylic paint on canvas",
+        "size": "20 x 20 inches"
+      },
+      {
+        "name": "Square Dance",
+        "img": "./img/squareDance.jpg",
+        "year": "2017",
+        "medium": "Acrylic paint on canvas",
+        "size": "20 x 20 inches"
+      }, 
+      {
+        "name": "Square Inch",
+        "img": "./img/squareInch.jpg",
+        "year": "2011",
+        "medium": "Acrylic paint on canvas",
+        "size": "20 x 20 inches"
+      }, 
+      {
+        "name": "Be There Or Be Square",
+        "img": "./img/beThereOrBeSquare.jpg",
+        "year": "2010",
+        "medium": "Acrylic paint on canvas",
+        "size": "24 x 30 inches"
+      }, 
+      {
+        "name": "Fair and Square I",
+        "img": "./img/fairAndSquareI.jpg",
+        "year": "2009",
+        "medium": "Acrylic paint on canvas",
+        "size": "20 x 24 inches"
+      }, 
+      {
+        "name": "Fair and Square II",
+        "img": "./img/fairAndSquareII.jpg",
+        "year": "2009",
+        "medium": "Acrylic paint on canvas",
+        "size": "30 x 40 inches"
+      }, 
+      {
+        "name": "Dark Night I",
+        "img": "./img/darkNightI.jpg",
+        "year": "2008",
+        "medium": "Acrylic paint on canvas",
+        "size": "20 x 20 inches"
+      }, 
+      {
+        "name": "Dark Night II",
+        "img": "./img/darkNightII.jpg",
+        "year": "2008",
+        "medium": "Acrylic paint on canvas",
+        "size": "20 x 20 inches"
+      }, 
+      {
+        "name": "Dark Night III",
+        "img": "./img/darkNightIII.jpg",
+        "year": "2008",
+        "medium": "Acrylic paint on canvas",
+        "size": "20 x 20 inches"
+      }, 
+      {
+        "name": "Dark Night IV",
+        "img": "./img/darkNightIV.jpg",
+        "year": "2008",
+        "medium": "Acrylic paint on canvas",
+        "size": "20 x 20 inches"
+      }
+    ],
+  "justBecause":
+    [
+      {
+        "name": "Get Happy",
+        "img": "./img/getHappy.jpg",
+        "year": "2018",
+        "medium": "Acrylic paint on canvas",
+        "size": "24 x 36 inches"
+      }
+    ]
+}
 
-  {
-    "name": "Square One II",
-    "img":  "./img/squareOneII.jpg",
-    "year": "2018",
-    "medium": "Acrylic paint on canvas",
-    "size": "40 x 40 inches"
-  },
 
-  {
-    "name": "Get Happy",
-    "img": "./img/getHappy.jpg",
-    "year": "2018",
-    "medium": "Acrylic paint on canvas",
-    "size": "24 x 36 inches"
-  }, 
-  {
-    "name": "Square Dance",
-    "img": "./img/squareDance.jpg",
-    "year": "2017",
-    "medium": "Acrylic paint on canvas",
-    "size": "20 x 20 inches"
-  }, 
-  {
-    "name": "Square Inch",
-    "img": "./img/squareInch.jpg",
-    "year": "2011",
-    "medium": "Acrylic paint on canvas",
-    "size": "20 x 20 inches"
-  }, 
-  {
-    "name": "Be There Or Be Square",
-    "img": "./img/beThereOrBeSquare.jpg",
-    "year": "2010",
-    "medium": "Acrylic paint on canvas",
-    "size": "24 x 30 inches"
-  }, 
-  {
-    "name": "Square One I",
-    "img": "./img/squareOneI.jpg",
-    "year": "2010",
-    "medium": "Acrylic paint on canvas",
-    "size": "20 x 20 inches"
-  }, 
-  {
-    "name": "Fair and Square I",
-    "img": "./img/fairAndSquare.jpg",
-    "year": "2009",
-    "medium": "Acrylic paint on canvas",
-    "size": "20 x 24 inches"
-  }, 
-  {
-    "name": "Fair and Square II",
-    "img": "./img/fairAndSquareII.jpg",
-    "year": "2009",
-    "medium": "Acrylic paint on canvas",
-    "size": "30 x 40 inches"
-  }, 
-  {
-    "name": "Dark Night I",
-    "img": "./img/darkNightI.jpg",
-    "year": "2008",
-    "medium": "Acrylic paint on canvas",
-    "size": "20 x 20 inches"
-  }, 
-  {
-    "name": "Dark Night II",
-    "img": "./img/darkNightII.jpg",
-    "year": "2008",
-    "medium": "Acrylic paint on canvas",
-    "size": "20 x 20 inches"
-  }, 
-  {
-    "name": "Dark Night III",
-    "img": "./img/darkNightIII.jpg",
-    "year": "2008",
-    "medium": "Acrylic paint on canvas",
-    "size": "20 x 20 inches"
-  }, 
-  {
-    "name": "Dark Night IV",
-    "img": "./img/darkNightIV.jpg",
-    "year": "2008",
-    "medium": "Acrylic paint on canvas",
-    "size": "20 x 20 inches"
-  }
-]
-
-
-var myPhoto = ["./img/redSquare.jpg","./img/squareOneII.jpg"];
 
 var container = document.getElementById("paintings_container");
-
-var table = document.createElement("table");
-document.getElementById("paintings_container").appendChild(table);
-
+paintings = all_paintings["colorWeave"]
 for (var i=0, len = paintings.length; i < len; ++i) {
     var painting = document.createElement("div");
     painting.className = "frame";
@@ -144,21 +144,6 @@ for (var i=0, len = paintings.length; i < len; ++i) {
 
     painting.appendChild(desc);
 
-    document.getElementById("paintings_container").appendChild(painting);
-
-    // name.appendChild(document.createTextNode(names[i]));
-    // name.innerHTML = names[i];
-    
-    // row.appendChild(name);
+    container.appendChild(painting);
 }
 
-
-  //   <div class="container">
-  //     <img class = "painting" src="img/cabo.jpg" alt="">
-  //   <div class = "desc">
-  //     <div><p class="i">Cabo</p></div> 
-  //     <div>2003</div>
-  //     <div>Acrylic paint and medium on canvas</div>
-  //     <div>24 x 30 inches</div>
-  //   </div>
-  // </div>
