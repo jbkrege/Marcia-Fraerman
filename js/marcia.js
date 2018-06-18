1cacd39bf18c366671eac94661484e832e25b30f
Ben Krege <jbkrege@gmail.com>
2018-06-18 17:12:21 -0500

    added dropdown

--- js/marcia.js
+++ js/marcia.js
@@ -16,13 +16,6 @@ all_paintings = {
   "colorWeave": 
     [ 
       {
-        "name": "Square One I",
-        "img": "./img/squareOneI.jpg",
-        "year": "2010",
-        "medium": "Acrylic paint on canvas",
-        "size": "20 x 20 inches"
-      }, 
-      {
         "name": "Square One II",
         "img":  "./img/squareOneII.jpg",
         "year": "2018",
@@ -56,6 +49,13 @@ all_paintings = {
         "year": "2010",
         "medium": "Acrylic paint on canvas",
         "size": "24 x 30 inches"
+      }, 
+      {
+        "name": "Square One I",
+        "img": "./img/squareOneI.jpg",
+        "year": "2010",
+        "medium": "Acrylic paint on canvas",
+        "size": "20 x 20 inches"
       }, 
       {
         "name": "Fair and Square I",
@@ -113,37 +113,75 @@ all_paintings = {
 }
 
 
+function camelize(str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+    return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+  }).replace(/\s+/g, '');
+}
+
+var era = ["Color Weave","Just Because"];
 
 var container = document.getElementById("paintings_container");
-paintings = all_paintings["colorWeave"]
-for (var i=0, len = paintings.length; i < len; ++i) {
-    var painting = document.createElement("div");
-    painting.className = "frame";
 
-    var photo = document.createElement("div");
-    img = new Image();
-    img.src = paintings[i]["img"];
-    img.alt = paintings[i]["name"];
-    img.className = "painting";
-    photo.appendChild(img);
-    painting.appendChild(img);
+// <li class = "nav-item"><a class="nav-link">x</a></li>
+// <li class = "nav-item"><a class="nav-link">y</a></li>
+// <li class = "nav-item"><a class="nav-link">z</a></li>
 
-    var desc = document.createElement("div");
-    desc.className = "desc";
+for (var e = 0, elen = era.length; e < elen; ++e){
+  // Add era to dropdown menu
+  var dropdown = document.getElementById("drop");
+  var li = document.createElement("li");
+  li.className = "nav-item";
+  var link = document.createElement("a");
+  link.className = "nav-link";
+  link.href = "#"+camelize(era[e]);
+  link.innerHTML = era[e]
+  li.appendChild(link);
+  dropdown.appendChild(li);
 
-    var title = document.createElement("div");
-    var text = document.createElement("p");
-    text.className = "i";
-    text.innerHTML = paintings[i]["name"];
-    title.appendChild(text);
-    desc.appendChild(title);
+  // Add anchor
+  var anchor = document.createElement("a")
+  anchor.name = camelize(era[e]);
+  container.appendChild(anchor);
 
-    var rest = document.createElement("div");
-    rest.innerHTML = ", "+paintings[i]["year"]+", "+paintings[i]["medium"]+", "+paintings[i]["size"];
-    desc.appendChild(rest);
+  // Add subheading
+  var subheading = document.createElement("h3");
+  subheading.className = "subheading";
+  subheading.innerHTML = era[e];
+  container.appendChild(subheading);
+  paintings = all_paintings[camelize(era[e])];
 
-    painting.appendChild(desc);
+  // Add paintings to dom
+  for (var i = 0, len = paintings.length; i < len; ++i) {
 
-    container.appendChild(painting);
+      var painting = document.createElement("div");
+      painting.className = "frame";
+
+      var photo = document.createElement("div");
+      img = new Image();
+      img.src = paintings[i]["img"];
+      img.alt = paintings[i]["name"];
+      img.className = "painting";
+      photo.appendChild(img);
+      painting.appendChild(img);
+
+      var desc = document.createElement("div");
+      desc.className = "desc";
+
+      var title = document.createElement("div");
+      var text = document.createElement("p");
+      text.className = "i";
+      text.innerHTML = paintings[i]["name"];
+      title.appendChild(text);
+      desc.appendChild(title);
+
+      var rest = document.createElement("div");
+      rest.innerHTML = ", "+paintings[i]["year"]+", "+paintings[i]["medium"]+", "+paintings[i]["size"];
+      desc.appendChild(rest);
+
+      painting.appendChild(desc);
+
+      container.appendChild(painting);
+  }
 }
 
