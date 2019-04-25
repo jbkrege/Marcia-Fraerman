5d8e7b198d7ebb49d8909e5572af53787e2592bf
Marcia Fraerman <marciafraerman@Thomass-iMac.local>
2019-04-25 11:40:22 -0500

    test from Marcia

--- js/marcia.js
+++ js/marcia.js
@@ -26,6 +26,7 @@
 //   }
 // });
 
+
 function camelize(str) {
   return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
     return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
